1c6ec84c4289d67c5260c43aa699bd1017332627
Tyler Hill <tylergenehill122@gmail.com>
2022-09-03 20:35:15 -0400

    import urls

--- src/app.py
+++ src/app.py
@@ -1,44 +1,5 @@
 from flask import Flask, render_template
-
-index_urls = {
-    'all_movies':'/static/Resources/all-movies.png',
-    'by_genre':'/static/Resources/avg_rating_by_genre.png',
-    'movies_per_genre':'/static/Resources/num_movies_per_genre.png'
-}
-
-genre_urls = {
-    'action':'/static/Resources/genre/average_rating_action.png',
-    'avg_action':'/static/Resources/genre/avg_rating_by_year_action.png',
-    'avg_crime':'/static/Resources/genre/avg_rating_by_year_crime.png',
-    'avg_drama':'/static/Resources/genre/avg_rating_by_year_drama.png',
-    'avg_war':'/static/Resources/genre/avg_rating_by_year_war.png',
-}
-
-release_urls = {
-    'all':'/static/Resources/by_release_year/all-movies.png',
-    'action':'/static/Resources/by_release_year/action-combined.png',
-    'all_genre':'/static/Resources/by_release_year/all_genre.png',
-}
-
-review_urls = {
-    'cult':'/static/Resources/by_review_year/cult-classics.png',
-    'disney':'/static/Resources/by_review_year/disney-classics.png',
-    'disney2':'/static/Resources/by_review_year/disney-classics2.png',
-    'thriller_mystery':'/static/Resources/by_review_year/Thriller-Mystery.png',
-    'animation_adventure':'/static/Resources/by_review_year/Animation-Adventure.png',
-    'war_action':'/static/Resources/by_review_year/War-Action.png'
-}
-
-ml_urls = {
-    'logreg':'/static/Resources/ml/logreg.png',
-    'ttt':'/static/Resources/ml/train-test-split.png',
-    'cluster1':'/static/Resources/ml/drama-vs-romance-1.png',
-    'elbow':'/static/Resources/ml/elbow.png',
-    'kmeans3':'/static/Resources/ml/kmeans-3.png',
-    'kmeans4':'/static/Resources/ml/kmeans-4.png',
-    'kmeans7':'/static/Resources/ml/kmeans-7.png',
-    'tsne':'/static/Resources/ml/drama-vs-romance-tsne.png',
-}
+from urls import index_urls, genre_urls, release_urls, review_urls, ml_urls
 
 app = Flask(__name__)
 
